5233f9d04d54cc3f7a51e80668c28a0ade600094
shivuparakanatti <103122119+shivuparakanatti@users.noreply.github.com>
2023-12-04 23:34:07 +0530

    Update Button.jsx

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -6,6 +6,7 @@ const Button = (styles) => {
      <button className={`bg-[#8fd8d8] px-4 py-2 rounded-xl mx-4 text-white ${styles}`}>Get Started</button>
     </div>
   )
+
 }
 
 export default Button
