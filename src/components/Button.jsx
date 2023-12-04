de3285542e6c3f0b52df95528098078725aa1f35
shivuparakanatti <103122119+shivuparakanatti@users.noreply.github.com>
2023-12-04 23:34:00 +0530

    Update Button.jsx

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -3,7 +3,6 @@ import React from 'react'
 const Button = (styles) => {
   return (
     <div>
-
      <button className={`bg-[#8fd8d8] px-4 py-2 rounded-xl mx-4 text-white ${styles}`}>Get Started</button>
     </div>
   )
